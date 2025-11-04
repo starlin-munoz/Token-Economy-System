60d3014100b51b9e54540f4b1dac91e87e1bb786
Starlin <starlinmunoz025@gmail.com>
2025-11-03 22:44:20 -0500

    Backend Deployement

--- Token_Economy/frontend/src/components/ProtectedRoute.jsx
+++ Token_Economy/frontend/src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 import api from '../api';
 import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants';
 import { useState, useEffect } from 'react';
@@ -14,17 +14,14 @@ function ProtectedRoute({ children }) {
     const refreshToken = async () => {
         const refreshToken = localStorage.getItem(REFRESH_TOKEN);
         try {
-            const res = await api.post('/api/token/refresh', { 
-                refresh: refreshToken,
-            });
-            if(res.status === 200) {
+            const res = await api.post('/api/token/refresh', { refresh: refreshToken });
+            if (res.status === 200) {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 setIsAuthorized(true);
-            }else{
+            } else {
                 setIsAuthorized(false);
             }
         } catch (error) {
-            console.log(error);
             setIsAuthorized(false);
         }
     };
